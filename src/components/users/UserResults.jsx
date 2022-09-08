21a6dfaeaa26804d04ea977377788438fefa591a
Dovran <dovranrustemov@gmail.com>
2022-09-08 19:01:29 -0400

    Added Redux

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+
+function UserResults() {
+  useEffect(() => {}, [])
+
+  const fetchUsers = async () => {
+    const response = await fetch()
+  }
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default UserResults
